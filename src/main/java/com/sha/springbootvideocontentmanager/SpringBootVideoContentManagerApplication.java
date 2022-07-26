e4da0a72e945430663ba2829218b0c9c90008cab
Bedirhan Buyukoz <bedirbuyukoz@gmail.com>
2022-07-26 15:02:07 +0300

    updated prod

--- src/main/java/com/sha/springbootvideocontentmanager/SpringBootVideoContentManagerApplication.java
+++ src/main/java/com/sha/springbootvideocontentmanager/SpringBootVideoContentManagerApplication.java
@@ -2,9 +2,11 @@ package com.sha.springbootvideocontentmanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 
 @SpringBootApplication
+@PropertySource("classpath:application-${spring.profiles.active:default}.properties")
 public class SpringBootVideoContentManagerApplication {
 
 	public static void main(String[] args) {
